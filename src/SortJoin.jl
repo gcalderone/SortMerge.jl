a21bbba7f65b5bb3226d022638ce492be51989a1
Giorgio Calderone <giorgio.calderone@gmail.com>
2018-07-16 18:55:50 +0200

    Minor changes

--- src/SortJoin.jl
+++ src/SortJoin.jl
@@ -1,3 +1,6 @@
+__precompile__(true)
+
+
 module SortJoin
 
 import Base.show
@@ -10,6 +13,8 @@ struct sortjoinResult
     sort2::Vector{Int}
     match1::Vector{Int}
     match2::Vector{Int}
+    unique1::Int
+    unique2::Int
     unmatch1::Vector{Int}
     unmatch2::Vector{Int}
     elapsed::Float64
@@ -17,20 +22,20 @@ end
 
 
 function show(stream::IO, j::sortjoinResult)
-    println(stream, "Len. array 1 : ", j.size1)
-    println(stream, "Len. array 2 : ", j.size2)
-    println(stream, "Matched items: ", length(j.match1))
-    println(stream, "Elapsed time : ", j.elapsed, " s")   
+    @printf("Len. array 1 : %-12d  matched: %-12d (%5.1f%%)\n", j.size1, j.unique1, 100.*j.unique1/float(j.size1))
+    @printf("Len. array 2 : %-12d  matched: %-12d (%5.1f%%)\n", j.size2, j.unique2, 100.*j.unique2/float(j.size2))
+    @printf("Matched items: %-12d\n", length(j.match1))
+    @printf("Elapsed time : %-8.4g s", j.elapsed)
 end
 
 
 # Default signdiff function
-function signdiff(vec1, vec2, i1, i2)
+function signdiff(sorting, vec1, vec2, i1, i2)
     return sign(vec1[i1] - vec2[i2])
 end
 
 
-function sortjoin(vec1, vec2, signdiff=signdiff, args...; skipsort=false)
+function sortjoin(vec1, vec2, signdiff=signdiff, args...; skipsort=false, verbose=false)
     elapsed = (Base.time_ns)()
     size1 = size(vec1)[1]
     size2 = size(vec2)[1]
@@ -38,18 +43,27 @@ function sortjoin(vec1, vec2, signdiff=signdiff, args...; skipsort=false)
     sort1 = Int.(linspace(1, size1, size1))
     sort2 = Int.(linspace(1, size2, size2))
     if !skipsort
-        sort1 = sortperm(sort1, lt=(i, j) -> (signdiff(vec1, vec1, i, j, args...) == -1))
-        sort2 = sortperm(sort2, lt=(i, j) -> (signdiff(vec2, vec2, i, j, args...) == -1))
+        sort1 = sortperm(sort1, lt=(i, j) -> (signdiff(true, vec1, vec1, i, j, args...)::Int == -1))
+        sort2 = sortperm(sort2, lt=(i, j) -> (signdiff(true, vec2, vec2, i, j, args...)::Int == -1))
     end
 
     i2a = 1
-    count = 0
     match1 = Array{Int}(0)
     match2 = Array{Int}(0)
+    completed = 0.
+    lastlog = -1.
     for i1 in 1:size1
         for i2 in i2a:size2
-            count += 1
-            sign::Int = signdiff(vec1, vec2, sort1[i1], sort2[i2], args...) # sign(vec1 - vec2)
+            # Logging
+            completed = ceil(1000. * ((i1-1) * size2 + i2) / (size1 * size2))
+            if verbose
+                if completed > lastlog
+                    @printf("Completed: %5.1f%%, matched: %d \r", completed/10., length(match1))
+                    lastlog = completed
+                end
+            end
+
+            sign::Int = signdiff(false, vec1, vec2, sort1[i1], sort2[i2], args...) # sign(vec1 - vec2)
             if sign == 0
                 push!(match1, i1)
                 push!(match2, i2)
@@ -61,18 +75,25 @@ function sortjoin(vec1, vec2, signdiff=signdiff, args...; skipsort=false)
             end
         end
     end
+    if verbose
+        @printf("Completed: %5.1f%%, matched: %d \n", 100., length(match1))
+    end
     match1 = sort1[match1]
     match2 = sort2[match2]
 
+    
     unmatch1 = fill(true, size1)
     unmatch2 = fill(true, size2)
-    unmatch1[unique(match1)] = false
-    unmatch2[unique(match2)] = false
+    u1 = unique(match1)
+    u2 = unique(match2)
+    unmatch1[u1] = false
+    unmatch2[u2] = false
     unmatch1 = find(unmatch1)
     unmatch2 = find(unmatch2)
     elapsed = (Base.time_ns)() - elapsed
 
-    return sortjoinResult(size1, size2, sort1, sort2, match1, match2, unmatch1, unmatch2, elapsed / 1.e9)
+    return sortjoinResult(size1, size2, sort1, sort2, match1, match2, length(u1), length(u2), 
+                          unmatch1, unmatch2, elapsed / 1.e9)
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -17,14 +17,24 @@ j = sortjoin(a1, a2)
 @test j.match2[2] == 5
 
 # --------------------------------------------------------------------
-nn = 100000
+nn = 10000
 a1 = rand(1:nn, nn)
 a2 = rand(1:nn, nn)
 
 j = sortjoin(a1, a2)
 @test sum(abs.(a1[j.match1] .- a2[j.match2])) == 0
+for i1 in j.unmatch1
+	for i2 in j.unmatch2
+		@test a1[i1] != a2[i2]
+	end
+end
 
 sort!(a1)
 sort!(a2)
 j = sortjoin(a1, a2, skipsort=true)
 @test sum(abs.(a1[j.match1] .- a2[j.match2])) == 0
+for i1 in j.unmatch1
+	for i2 in j.unmatch2
+		@test a1[i1] != a2[i2]
+	end
+end
